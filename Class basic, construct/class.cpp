21d111bbdfb87ef0771c258dd05bea6b22f49ccb
ImKingMin <87991841+ImKingmMin@users.noreply.github.com>
2021-08-02 17:52:16 +0900

    Add files via upload

---
+++ Class basic, construct/class.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//using namespace std;
+//
+//class Circle {
+//public :
+//	int radius;
+//	Circle();      // 위임생성자
+//	Circle(int r); // 타겟생성자
+//	double getArea();
+//};
+//
+//Circle::Circle() :Circle(1){}
+//
+//Circle::Circle(int r)
+//{
+//	radius = r;
+//    cout << "반지름 " << radius << "원 생성" << endl;
+//}
+//
+//double Circle::getArea() {
+//	return 3.14*radius*radius;
+//}
+//
+//int main() {
+//	Circle donut;   // 매개변수없는 생성자 호출
+//	double area = donut.getArea();  
+//	cout << "Donut 면적은 " << area << endl;
+//
+//	Circle pizza(30);
+//	area = pizza.getArea();
+//	cout << "Pizza 면적은 " << area << endl;
+//}
